f545a605bd47a79692415a75524ed29987230073
Ángeles Elsa Wehle <112911469+AEWehle@users.noreply.github.com>
2023-06-07 08:53:36 -0300

    Actualizar time_for_food.cpp

--- modules/time_for_food/time_for_food.cpp
+++ modules/time_for_food/time_for_food.cpp
@@ -13,6 +13,8 @@
 
 //=====[Declaration of private defines]========================================
 
+#define int food_time_t
+
 //=====[Declaration of private data types]=====================================
 
 //=====[Declaration and initialization of public global objects]===============
@@ -23,7 +25,7 @@
 
 const int MAX_TIMES_DAY = 50;
 static int timesIndex = 2;
-static int times_for_food[MAX_TIMES_DAY] = { 48 , 120 }; // horarios cada 10 minutos desde 00:00 a 23:50, opción desde 0 a 143 
+static food_time_t times_for_food[MAX_TIMES_DAY] = { 48 , 120 }; // horarios cada 10 minutos desde 00:00 a 23:50, opción desde 0 a 143 
 // hora =  (número // 6), div entera de 6
 // minutos = (número % 6) *60, el resto*60
 
@@ -35,10 +37,21 @@ const float FOOD_LOAD_DEFAULT = 100;//g
 
 //=====[Declarations (prototypes) of private functions]========================
 
+void sort_times();
+
+//=====[Implementations of public functions]===================================
+
+void time_for_food_init()
+{    
+    food_mode = CLOSED; // Por default e entrega comida hasta que haya MAX_FOOD_LOAD en el bowl a las 8hs y a las 20hs
+    set_max_food_load( FOOD_LOAD_DEFAULT );
+}
+
+
 // devuelve true si actual time es un time_for_food
 bool its_time( char* actual_time )
 {
-    int time_numer = atoi( actual_time[11] ) * 10 + atoi( actual_time[12] ) + atoi( actual_time[14] ) * 10 + atoi( actual_time[15] ); // (hora*6 + minuto)
+    food_time_t time_numer = atoi( actual_time[11] ) * 10 + atoi( actual_time[12] ) + atoi( actual_time[14] ) * 10 + atoi( actual_time[15] ); // (hora*6 + minuto)
     for( int i = 0; i < timesIndex ; i++){
         if ( time_numer == times_for_food[i] ) 
             return true;
@@ -47,40 +60,60 @@ bool its_time( char* actual_time )
 }
 
 
-//=====[Implementations of public functions]===================================
-
-void time_for_food_init()
-{    
-    food_mode = CLOSED; // se entrega comida hasta que en el tacho haya MAX_FOOD_LOAD en el bowl
-    set_max_food_load( FOOD_LOAD_DEFAULT );
-}
-
 void time_for_food_update()
 {
     char* actual_time = dateAndTimeRead(); //  devuelve "Sun Sep 16 01:03:52 1973\n\0"
-    if( its_time( actual_time, times_for_food[0] ) ){
+    if( its_time( actual_time ){
         bowl_charge();
     }
 }
 
-void add_time_for_food( char new_time[6] ){
-     if ( timesIndex < MAX_TIMES_DAY )
-     {
-        timesIndex++;
-        for ( int i = 0; i<6 ; i++ )
+
+void add_food_time( food_time_t new_time ){
+      bool found = false;
+     for ( int i = 0 ; i < timesIndex && !found ; i++ )
+    {
+        if ( times_for_food[ i ] == new_time )
         {
-        times_for_food[timesIndex][I] = new_ime[i]
+           found = true;
         }
-     }
+    }
+    if (!found) {
+           timesIndex++;
+           times_for_food[timesIndex]= new_ime;
+}
 }
 
-void change_a_time( char time[6], int index ){
-     for ( int i = 0; i<6 ; i++ )
-     {
-     times_for_food[index][i] = time[i];
-     }
+
+void change_food_time( food_time_t new_time, food_time_t old_time ){
+     bool found = false;
+     for ( int i = 0 ; i < timesIndex && !found ; i++ )
+    {
+        if ( times_for_food[ i ] == old_tine )
+        {
+           found = true;
+           times_for_food[i] = new_time;
+        }
+    }
+}
+
+
+void delete_food_time( food_time_t bad_time )
+{
+    bool found = false;
+    for ( int i = 0 ; i < (timesIndex-1) ; i++ )
+    {
+        if ( times_for_food[ i ] == bad_time )
+           found = true;
+        if ( found )
+        times_for_food[ i ] = times_for_food[i+1];
+    }
 }
 
 
 
 //=====[Implementations of private functions]==================================
+
+void sort_times(){
+     sort(times_for_food, times_for_food + timesIndex);
+}
